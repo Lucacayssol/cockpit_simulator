856df7fabbc415e8339668bf9ca2aa15c7cc12de
Luca Cayssol <luca.cayssol@gmail.com>
2026-09-19 18:05:51 +0200

    Add joystick control for X-axis input
    Initialize joystick and read analog input for X-axis.

--- Rudder/sketch_rudder/sketch_rudder.ino
+++ Rudder/sketch_rudder/sketch_rudder.ino
@@ -1,1 +1,39 @@
+ #include <Joystick.h>
 
+// Crée une manette USB avec un axe X
+Joystick_ Joystick(
+  JOYSTICK_DEFAULT_REPORT_ID,
+  JOYSTICK_TYPE_JOYSTICK,
+  0,     // boutons
+  0,     // hats
+  true,  // Axe X
+  false, false,
+  false, false, false,
+  false, false, false, false, false
+);
+
+const int pin = A0;
+
+void setup() {
+  pinMode(pin, INPUT);
+
+  // Valeur de l'axe : 0 à 1023
+  Joystick.setXAxisRange(0, 1023);
+
+  Joystick.begin();
+}
+
+void loop() {
+  int valeur = analogRead(pin);
+  Serial.println(valeur);
+  valeur = map(valeur,415,665,0,1023);
+  valeur = 1023 - valeur;
+  valeur = constrain(valeur,0,1023);
+  if (valeur > 462 and valeur < 562) {
+    valeur = 512;
+  }
+  Joystick.setXAxis(valeur);
+  Serial.println(valeur);
+  Serial.println(" ");
+  delay(5);
+}
